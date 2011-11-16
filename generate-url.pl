3a208854e2a8b463872e82f411c76e009dd0c23e
Shugo Numano <numano@cc.rim.or.jp>
2011-11-16 11:14:48 +0900

    *use URI::Encode module

--- generate-url.pl
+++ generate-url.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# by snumano 2011/11/15
+# by snumano
 
 use strict;
 use warnings;
@@ -11,13 +11,14 @@ use MIME::Base64;
 use WWW::Mechanize;
 use Encode;
 use XML::Twig;
+use URI::Encode;
 
 my $output;
 my ($field,$value);
 
 my $my_filename = basename($0, '');
 our($opt_u,$opt_a,$opt_s);
-my $site = "http://*.*.*.*/client/api?";
+my $site = "http://*.*.*.*/client/api?"; # Pls change your Base URL & API Path
 getopt "uas";
 
 if(!defined($opt_u) || !defined($opt_a) || !defined($opt_s)){
@@ -27,40 +28,31 @@ my $command = $opt_u;
 my $api_key = $opt_a;
 my $secret_key = $opt_s;
 
-### Generate URL ###
+my $uri = URI::Encode->new();
 
-#
+### Generate URL ###
 #step1
-#
+
 my $query = $command."&apiKey=".$api_key;
 my @list = split(/&/,$query);
 foreach  (@list){
-    if(/(.+)\=(.+)/){
+	if(/(.+)\=(.+)/){
 		$field = $1;
-		$value = &url_encode($2);
+		$value = $uri->encode($2, 1); # encode_reserved option is set to 1
 		$_ = $field."=".$value;
 	}
 }
 
-#
 #step2
-#
 foreach  (@list){
 	$_ = lc($_);
 }
 $output = join("&",sort @list);
 
-#
 #step3
-#
-my $digest = hmac_sha1($output, $secret_key);
-#print "DIGEST:".$digest."\n";    
-my $base64_encoded = encode_base64($digest);
-chomp($base64_encoded);
-#print "BASE64 ENCODED:".$base64_encoded."\n";    
-my $url_encoded = &url_encode($base64_encoded);   # this url encode is need
-#print "URL ENCODED:".$url_encoded."\n";    
-
+my $digest = hmac_sha1($output, $secret_key);   
+my $base64_encoded = encode_base64($digest);chomp($base64_encoded);
+my $url_encoded = $uri->encode($base64_encoded, 1); # encode_reserved option is set to 1
 my $url = $site."apikey=".$api_key."&".$command."&signature=".$url_encoded;
 
 print "\nGenerate URL...\n".$url."\n\n";    
@@ -69,7 +61,6 @@ print "\nGenerate URL...\n".$url."\n\n";
 ### get URL ###
 my $mech = WWW::Mechanize->new();
 $mech->get($url);
-
 my $xml = encode('cp932',$mech->content);
 
 my $twig = XML::Twig->new(pretty_print => 'indented', );
@@ -77,13 +68,3 @@ $twig->parse($xml);
 $twig->print;
 
 exit;
-
-
-### sub routine ###
-
-sub url_encode {
-    my $str = shift;
-    $str =~ s/([^\w -\._~])/'%'.unpack('H2', $1)/eg;
-	$str =~ tr/ /%20/;	# space shuld be translated to %20.
-    return $str;
-}
